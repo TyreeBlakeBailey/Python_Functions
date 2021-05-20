446a129066a3fa9112ba1cc7bec47241e34a5570
Tyree <t9tyree@hotmail.co.uk>
2021-05-20 16:05:43 +0100

    add, multiple, divide, functions

--- python_functions.py
+++ python_functions.py
@@ -1,18 +1,36 @@
-# Let's create a basic functions to greet
-# syntax def name()
+# # Let's create a basic functions to greet
+# # syntax def name()
+#
+#
+# # first iteration
+#
+# def greeting():
+#     return "Welcome to Cyber Security! "  # last line in function should be a return statment
+#
+#
+# print(greeting())  # function has to be called in order to run
+#
+#
+# ##Second iteration
+#
+# def greeting_user(name):
+#     return f"Welcome to the team {name}"
+#
+#
+# print(greeting_user(input("What is your name? ").capitalize()))
+
+##create a function that takes two args as ints
 
 
-#first iteration
+def add(Num1, Num2):
+    return Num1 + Num2
 
-def greeting():
-    return "Welcome to Cyber Security! " # last line in function should be a return statment
+def multiple(Num1, Num2):
+    return Num1 * Num2
 
-print(greeting())#function has to be called in order to run
+def divide(Num1, Num2):
+    return Num1/Num2
 
-
-##Second iteration
-
-def greeting_user(name):
-    return f"Welcome to the team {name}"
-
-print(greeting_user(input("What is your name? ").capitalize()))
+print(add(4,6))
+print(multiple(3,8))
+print(divide(10,2))
